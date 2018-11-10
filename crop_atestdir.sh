8a5092ab58799dfcef2970d4888d0dc9dba502fb
Benoit Brummer <trougnouf@gmail.com>
2018-11-10 18:00:29 +0100

    test.py seems done

--- crop_atestdir.sh
+++ crop_atestdir.sh
@@ -10,6 +10,7 @@
 # args
 DN=$2	# dirname
 CS=$1	# crop size
+IMG=$3
 
 
 if ! [[ "$CS" =~ ^[0-9]+$ ]] || ((CS%8!=0))
@@ -19,39 +20,45 @@ then
 	exit -1
 fi
 
-images=($(ls ${DN}))
-for IMG in ${images[@]}
-	do
-	echo "${IMG}"
-	RES=($(file ${DN}/${IMG} | grep -o -E '[0-9]{4,}x[0-9]{3,}' | grep -o -E '[0-9]+'))
-	BASENAME="${IMG%.*}"
-	ISO=$(grep -o 'ISOH*[0-9]*' <<< ${IMG})
-	if [ ! "${ISO}" ]
-	then
-	    ISO=UNK
-	fi
-	mkdir -p "${DN}_${CS}/${ISO}"
-	
-	let CURY=CURX=CROPCNT=0
-	while (("$CURY"<${RES[1]}))
-	do
-		CROPPATH="${DN}_${CS}/${ISO}/${BASENAME}_${CROPCNT}.jpg"
-		if [ ! -f "${CROPPATH}" ]
-		then
-			echo "jpegtran -crop ${CS}x${CS}+${CURX}+${CURY} -copy none -trim -optimize -outfile ${CROPPATH} ${DN}/${IMG}"
-			jpegtran -crop ${CS}x${CS}+${CURX}+${CURY} -copy none -trim -optimize -outfile ${CROPPATH} ${DN}/${IMG}
-		fi
-		((CROPCNT++))
-		((CURX+=CS))
-		if ((CURX+CS>${RES[0]}))
-		then
-			CURX=0
-			((CURY+=CS))
-			if ((CURY+CS>${RES[1]}))
-			then
-				echo "${DN} cropped into $(((CROPCNT+1))) pieces."
-			fi
-		fi
+if [ ! "$IMG" ]
+then
+    NTHREADS=$(grep -c ^processor /proc/cpuinfo)
+    echo "Running with $NTHREADS threads..."
+    ls ${DN} | xargs --max-procs=${NTHREADS} -n 1 bash $0 $1 $2
+    exit
+fi
 
-	done
+
+echo "${IMG}"
+RES=($(file ${DN}/${IMG} | grep -o -E '[0-9]{4,}x[0-9]{3,}' | grep -o -E '[0-9]+'))
+BASENAME="${IMG%.*}"
+ISO=$(grep -o 'ISOH*[0-9]*' <<< ${IMG})
+if [ ! "${ISO}" ]
+then
+    ISO=UNK
+fi
+mkdir -p "${DN}_${CS}/${ISO}"
+
+let CURY=CURX=CROPCNT=0
+while (("$CURY"<${RES[1]}))
+do
+	CROPPATH="${DN}_${CS}/${ISO}/${BASENAME}_${CROPCNT}.jpg"
+	if [ ! -f "${CROPPATH}" ]
+	then
+		#echo "jpegtran -crop ${CS}x${CS}+${CURX}+${CURY} -copy none -trim -optimize -outfile ${CROPPATH} ${DN}/${IMG}"
+		jpegtran -crop ${CS}x${CS}+${CURX}+${CURY} -copy none -trim -optimize -outfile ${CROPPATH} ${DN}/${IMG}
+	fi
+	((CROPCNT++))
+	((CURX+=CS))
+	if ((CURX+CS>${RES[0]}))
+	then
+		CURX=0
+		((CURY+=CS))
+		if ((CURY+CS>${RES[1]}))
+		then
+			echo "${DN} cropped into $(((CROPCNT+1))) pieces."
+		fi
+	fi
+
 done
+
