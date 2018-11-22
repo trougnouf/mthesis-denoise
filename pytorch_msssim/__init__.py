a06ed9dc91db80e48587aaa514cadc199cdc2b5e
trougnouf <trougnouf@gmail.com>
2018-11-22 16:04:33 +0100

    Attempting msssim

--- pytorch_msssim/__init__.py
+++ pytorch_msssim/__init__.py
@@ -130,6 +130,6 @@ class MSSSIM(torch.nn.Module):
 
     def forward(self, img1, img2):
         # TODO: store window between calls if possible
-        ret= 1-msssim(img1, img2, window_size=self.window_size, size_average=self.size_average)
+        ret= -msssim(img1, img2, window_size=self.window_size, size_average=self.size_average)
         print(ret)
         return ret
