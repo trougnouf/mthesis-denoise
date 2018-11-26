f5984a2871725d6c574340a6220f76b578486385
Benoit Brummer <trougnouf@gmail.com>
2018-11-27 00:39:15 +0100

    fix kernel size

--- nnModules.py
+++ nnModules.py
@@ -4,7 +4,7 @@ import torch.nn.init as init
 class DnCNN(nn.Module):
     def __init__(self, depth=22, n_channels=64, image_channels=3, use_bnorm=True, kernel_size=3, find_noise=True):
         super(DnCNN, self).__init__()
-        padding = (kernel_size/2)-1
+        padding = int((kernel_size/2)-1)
         layers = []
         self.find_noise = find_noise
 
