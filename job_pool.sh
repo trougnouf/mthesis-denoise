0907f4e3d74bb57a5348d9f414edcdfef03b038a
trougnouf <trougnouf@gmail.com>
2018-11-27 08:34:15 +0100

    Add job queue

--- job_pool.sh
+++ job_pool.sh
@@ -1,2 +1,2 @@
-python3 run_nn.py --batch_size 36 --cuda_device 1 --n_channels 128 --kernel_size 5 --epoch 158
+python3 run_nn.py --batch_size 36 --cuda_device 1 --n_channels 128 --kernel_size 5 --epoch 76
 
