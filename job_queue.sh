a3eedbbb2cac3288da34f4ffc181b0b17090dea6
Benoit Brummer <benoit.brummer@student.uclouvain.be>
2018-11-28 15:21:20 +0100

    res

--- job_queue.sh
+++ job_queue.sh
@@ -1,2 +1,1 @@
-python3 run_nn.py --expname 2018-11-27T00:45_run_nn.py_--batch_size_36_--cuda_device_1_--n_channels_128_--kernel_size_5_--find_noise_0_--epoch_158 --batch_size 36 --cuda_device 1 --n_channels 128 --kernel_size 5 --epoch 76 
 python3 run_nn.py --batch_size 36 --cuda_device 1 --n_channels 128 --kernel_size 5 --epoch 76 --find_noise
