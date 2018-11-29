a4da86cdfb15a4f161d02ea51fb6a72923728189
trougnouf <trougnouf@gmail.com>
2018-11-29 12:19:22 +0100

    RedCNN

--- nnModules.py
+++ nnModules.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.init as init
+from math import floor
 
 class DnCNN(nn.Module):
     def __init__(self, depth=22, n_channels=64, image_channels=3, use_bnorm=True, kernel_size=3, find_noise=True):
@@ -42,26 +43,53 @@ class DnCNN(nn.Module):
                 init.constant_(m.bias, 0)
 
 class RedCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, n_channels=96, image_channels=3, depth=22):
         super(RedCNN, self).__init__()
-        layers = []
-        self.redcnn = nn.Sequential(*layers)
-        self.__initialize_weights()
+        self.depth = depth
+        self.conv_first = nn.Conv2d(in_channels=image_channels, out_channels=out_channels, kernel_size=5, stride=1, padding=0)
+        self.conv = nn.Conv2d(n_channels, n_channels, kernel_size=5, stride=1, padding=0)
+        self.deconv = nn.ConvTranspose2d(n_channels, n_channels, kernel_size=5, stride=1, padding=0)
+        self.deconv_last = nn.ConvTranspose2d(n_channels, n_channels=image_channels, kernel_size=5, stride=1, padding=0)
+        self.relu = nn.ReLU()
+
     def forward(self, x):
-        if self.find_noise:
-            y = x
-            out = self.dncnn(x)
-            return y-out
-        else:
-            out = self.dncnn(x)
-            return out
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.orthogonal_(m.weight)
-                print('init weight')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
+        residuals = []
+        residuals.append(x.clone())
+        layer = self.relu(self.conv_first(x))
+        for _ in range(floor((self.depth-4)/2)):
+            layer = self.relu(self.conv(layer))
+            layer = self.relu(self.conv(layer))
+            residuals.append(layer.clone())
+        layer = self.relu(self.conv(layer))
+        layer = self.deconv(layer)
+        layer += residuals.pop()
+        for _ in range(floor((self.depth-4)/2)):
+            layer = self.deconv(self.relu(layer))
+            layer = self.deconv(self.relu(layer))
+            layer += residuals.pop()
+        
+        layer = self.deconv_last(self.relu(layer))
+        layer += residuals.pop()
+        layer = self.relu(layer)
+        return layer
+        
+        # Encoder
+        residual1 = x.clone()
+        layer = self.relu(self.conv_first(x))
+        layer = self.relu(self.conv(layer))
+        residual2 = layer.clone()
+        layer = self.relu(self.conv(layer))
+        layer = self.relu(self.conv(layer))
+        residual3 = layer.clone()
+        layer = self.relu(self.conv(layer))
+        # decoder
+        layer = self.deconv(layer)
+        layer += residual3
+        layer = self.deconv(self.relu(layer))
+        layer = self.deconv(self.relu(layer))
+        layer += residual2
+        layer = self.deconv(self.relu(layer))
+        layer = self.deconv_last(self.relu(layer))
+        layer += residual1
+        layer = self.relu(layer)
+        return layer
