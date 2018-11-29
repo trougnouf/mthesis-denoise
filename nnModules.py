0699fd3a86ff2b51b88fbe12849f5cf37632465b
Benoit Brummer <trougnouf@gmail.com>
2018-11-29 20:10:54 +0100

    Trying RedishCNN

--- nnModules.py
+++ nnModules.py
@@ -42,6 +42,39 @@ class DnCNN(nn.Module):
                 init.constant_(m.weight, 1)
                 init.constant_(m.bias, 0)
 
+# based on https://arxiv.org/pdf/1603.05027.pdf ish
+class RedishCNN(nn.Module):
+    def __init__(self, n_channels=128, image_channels=3, kernel_size=5, depth=30):
+        super(RedCNN, self).__init__()
+        self.depth = depth
+        self.bn = nn.BatchNorm2d(n_channels)
+        self.conv_first = nn.Conv2d(in_channels=image_channels, out_channels=n_channels, kernel_size=kernel_size, stride=1, padding=0)
+        self.conv = nn.Conv2d(n_channels, n_channels, kernel_size=kernel_size, stride=1, padding=0)
+        self.deconv = nn.ConvTranspose2d(n_channels, n_channels, kernel_size=kernel_size, stride=1, padding=0)
+        self.deconv_last = nn.ConvTranspose2d(in_channels=n_channels, out_channels=image_channels, kernel_size=kernel_size, stride=1, padding=0)
+        self.relu = nn.RReLU()
+
+    def forward(self, x):
+        residuals = []
+        layer = self.relu(self.conv_first(x))   #c1
+        residuals.append(layer.clone())
+        for _ in range(int(floor(self.depth-6)/2)):
+            for _ in range(2):
+                layer = self.bn(layer)
+                layer = self.relu(layer)
+                layer = self.conv(layer)
+            residuals.append(layer.clone())
+        layer = self.relu(self.conv(layer))     #clast
+        layer = self.relu(self.deconv(layer))   #d1
+        layer = self.relu(layer+residuals.pop())
+        for _ in range(int(floor(self.depth-6)/2)):
+            for _ in range(2):
+                layer = self.bn(layer)
+                layer = self.relu(layer)
+                layer = self.deconv(layer)
+            layer = self.relu(layer+residuals.pop())
+        layer = self.relu(self.deconv_last(layer))
+        return layer
 
 class RedCNN(nn.Module):
     def __init__(self, n_channels=128, image_channels=3, kernel_size=5, depth=30):
