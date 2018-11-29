3f81eb4ca110ffb00364f479741764af88204526
Benoit Brummer <benoit.brummer@student.uclouvain.be>
2018-11-29 16:02:19 +0100

    [run_nn.py] implement resume, res

--- run_nn.py
+++ run_nn.py
@@ -53,7 +53,7 @@ torch.cuda.set_device(args.cuda_device)
 
 def find_experiment():
     exp = None
-    bname = ('_'.join(sys.argv).replace('/','-')).strip('_--resume')
+    bname = ('_'.join(sys.argv).replace('/','-')).replace('_--resume','')
     for adir in os.listdir(args.models_dir):
         if adir[17:]==bname:
             exp = adir
