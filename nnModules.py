7ab2afcdc473c4fdd92594a045af8fb9283b62af
trougnouf <trougnouf@gmail.com>
2018-11-29 13:51:24 +0100

    RedCNN

--- nnModules.py
+++ nnModules.py
@@ -43,53 +43,34 @@ class DnCNN(nn.Module):
                 init.constant_(m.bias, 0)
 
 class RedCNN(nn.Module):
-    def __init__(self, n_channels=96, image_channels=3, depth=22):
+    def __init__(self, n_channels=128, image_channels=5, depth=22):
         super(RedCNN, self).__init__()
         self.depth = depth
         self.conv_first = nn.Conv2d(in_channels=image_channels, out_channels=n_channels, kernel_size=5, stride=1, padding=0)
         self.conv = nn.Conv2d(n_channels, n_channels, kernel_size=5, stride=1, padding=0)
         self.deconv = nn.ConvTranspose2d(n_channels, n_channels, kernel_size=5, stride=1, padding=0)
         self.deconv_last = nn.ConvTranspose2d(in_channels=n_channels, out_channels=image_channels, kernel_size=5, stride=1, padding=0)
-        self.relu = nn.ReLU()
+        self.relu = nn.RReLU()
 
     def forward(self, x):
         residuals = []
         residuals.append(x.clone())
         layer = self.relu(self.conv_first(x))
-        for _ in range(floor((self.depth-4)/2)):
+        layer = self.relu(self.conv(layer))
+        residuals.append(layer.clone())
+        for _ in range(floor((self.depth-6)/2)):
             layer = self.relu(self.conv(layer))
             layer = self.relu(self.conv(layer))
             residuals.append(layer.clone())
         layer = self.relu(self.conv(layer))
-        layer = self.deconv(layer)
+        layer = self.relu(self.deconv(layer))
         layer += residuals.pop()
-        for _ in range(floor((self.depth-4)/2)):
-            layer = self.deconv(self.relu(layer))
-            layer = self.deconv(self.relu(layer))
+        for i in range(floor((self.depth-6)/2)):
+            layer = self.relu(self.deconv(layer))
+            layer = self.relu(self.deconv(layer))
             layer += residuals.pop()
-        
+        layer = self.relu(self.deconv(layer))
         layer = self.deconv_last(self.relu(layer))
         layer += residuals.pop()
-        layer = self.relu(layer)
-        return layer
-        
-        # Encoder
-        residual1 = x.clone()
-        layer = self.relu(self.conv_first(x))
-        layer = self.relu(self.conv(layer))
-        residual2 = layer.clone()
-        layer = self.relu(self.conv(layer))
-        layer = self.relu(self.conv(layer))
-        residual3 = layer.clone()
-        layer = self.relu(self.conv(layer))
-        # decoder
-        layer = self.deconv(layer)
-        layer += residual3
-        layer = self.deconv(self.relu(layer))
-        layer = self.deconv(self.relu(layer))
-        layer += residual2
-        layer = self.deconv(self.relu(layer))
-        layer = self.deconv_last(self.relu(layer))
-        layer += residual1
         layer = self.relu(layer)
         return layer
