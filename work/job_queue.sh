80b9bdfe1dc2d097445aa68440f88725190010cf
Benoit Brummer <benoit.brummer@student.uclouvain.be>
2018-12-05 18:49:40 +0100

    res

--- work/job_queue.sh
+++
@@ -1,3 +0,0 @@
-python3 run_nn.py --batch_size 36 --cuda_device 1 --n_channels 128 --kernel_size 5 --epoch 76 --docompression random
-python3 run_nn.py --batch_size 36 --cuda_device 1 --n_channels 128 --kernel_size 5 --epoch 76 --docompression 60
-python3 run_nn.py --batch_size 36 --cuda_device 1 --n_channels 128 --kernel_size 5 --epoch 38 --lr 0.0001 --random_lr
