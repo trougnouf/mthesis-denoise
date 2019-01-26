be91f2fb84c8734ddf6894b920df88eafaf59612
trougnouf (Benoit Brummer) <trougnouf@gmail.com>
2019-01-26 23:31:00 +0100

    [crop_ds.py] add SIDD support

--- crop_ds.py
+++ crop_ds.py
@@ -5,13 +5,16 @@ from multiprocessing import cpu_count
 parser = argparse.ArgumentParser(description='Image cropper with overlap (relies on crop_img.sh)')
 parser.add_argument('--cs', default=128, type=int, help='Crop size')
 parser.add_argument('--ucs', default=96, type=int, help='Useful crop size')
-parser.add_argument('--dsdir', default='datasets/dataset', type=str, help='Input dataset directory. Default is datasets/dataset, for test try datasets/noisyonly')
-parser.add_argument('--resdir', default='datasets/train', type=str, help='Output cropped dataset directory ([resdir]/ds_[cs]_[ucs]). Default is datasets/train, for test try datasets/test')
+parser.add_argument('--dsdir', default='datasets/NIND', type=str, help='Input dataset directory. Default is datasets/dataset, for test try datasets/noisyonly')
+parser.add_argument('--resdir', default='datasets/train', type=str, help='Output cropped dataset directory ([resdir]/dsdir_[cs]_[ucs]). Default is datasets/train, for test try datasets/test')
 parser.add_argument('--max_threads', type=int, help='Maximum number of active threads, default=#threads')
 args = parser.parse_args()
 
-resdir = os.path.join(args.resdir, 'ds_'+str(args.cs)+'_'+str(args.ucs))
+dsdir = args.dsdir.split('/')[-1]
+resdir = os.path.join(args.resdir, dsdir+'_'+str(args.cs)+'_'+str(args.ucs))
 todolist = []
+
+
 
 sets = os.listdir(args.dsdir)
 # structured dataset
@@ -19,7 +22,7 @@ if os.path.isdir(os.path.join(args.dsdir, sets[0])):
     for aset in sets:
         for image in os.listdir(os.path.join(args.dsdir, aset)):
             inpath=os.path.join(args.dsdir, aset, image)
-            isoval=image.split('_')[-1][:-4]
+            isoval=image.split('_')[0] if 'SIDD' in dsdir else image.split('_')[-1][:-4]
             outdir=os.path.join(resdir, aset, isoval)
             todolist.append(['bash', 'crop_img.sh', str(args.cs), str(args.ucs), inpath, outdir])
 # or simple image directory
