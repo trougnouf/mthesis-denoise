a54a18229456dfb018fc9292f4c67d263020de4b
Benoit Brummer <benoit.brummer@student.uclouvain.be>
2019-01-31 02:17:36 +0100

    res

--- work/job_queue.sh
+++
@@ -1,1 +0,0 @@
-python3 run_nn.py --train_data datasets/train/SIDD_128_96 datasets/train/NIND_128_96 --lr 3e-4 --cuda_device 2 --time_limit 259200 --batch_size 94 --test_reserve ursulines-red stefantiek ursulines-building MuseeL-Bobo CourtineDeVillersDebris
