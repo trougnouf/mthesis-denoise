aaa6abc0d54e371255cfb98d913720deb3d92180
Benoit Brummer <benoit.brummer@student.uclouvain.be>
2019-02-13 21:31:23 +0100

    res

--- work/job_queue.sh
+++ work/job_queue.sh
@@ -1,2 +1,1 @@
 python3 run_nn.py --train_data datasets/train/SIDDmed_128_96 datasets/train/NIND_128_96 --lossf MSE --lr 3e-4 --cuda_device 0 --time_limit 259200 --batch_size 94 # NIND+SIDDmed w/MSE for comp
-
