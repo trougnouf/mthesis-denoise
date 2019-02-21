0d0ec23482ace3b557a37c2ed1da92b60cdcc431
Benoit Brummer <benoit.brummer@student.uclouvain.be>
2019-02-21 22:38:58 +0100

    res

--- work/job_queue.sh
+++ work/job_queue.sh
@@ -1,1 +1,1 @@
-
+python3 run_nn.py --train_data datasets/train/SIDDmed_128_96 datasets/train/NIND_128_96 --lr 3e-4 --batch_size 68 --skip_sizecheck --test_reserve ursulines-red stefantiek ursulines-building MuseeL-Bobo CourtineDeVillersDebris MuseeL-Bobo-C500D
