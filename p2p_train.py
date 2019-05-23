78faec0a8313510c7fe94ef64e49371d1fd7d792
trougnouf <trougnouf@gmail.com>
2019-05-24 01:52:49 +0200

    maybe less confidence

--- p2p_train.py
+++ p2p_train.py
@@ -91,7 +91,7 @@ print(args)
 
 # requires Sigmoid
 def confident_mse_loss(answer, target):
-    not_confident_i = ((answer > 0.4) & (answer < 0.6)).to(torch.float32)
+    not_confident_i = ((answer > 0.45) & (answer < 0.55)).to(torch.float32)
     confident_i = 1-not_confident_i
     dif = torch.abs(answer-target)
     res = confident_i*dif**2+not_confident_i*dif
